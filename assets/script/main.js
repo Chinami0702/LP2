51486f669842fb73e6e2ccb223385b983ea8f115
Chinami0702 <chinamik0702@gmail.com>
2025-05-27 23:54:44 +0900

    jquery.css

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,54 +1,36 @@
 $(document).ready(function () {
-  $(".question").on("click", function () {
+  $(".qa_item_box").on("click", function () {
     console.log("クリックされた"); // ←これを追加
 
-    const answer = $(this).next(".answer");
+    const answer = $(this).next(".qa_answer");
     const btn = $(this).find(".toggle-btn");
 
     answer.slideToggle(300);
     btn.toggleClass("open");
   });
 });
-
-  //  $(document).ready(function(){
-  //     $('.main_review_container_box_box').slick({
-      
-  //   });
-  // });
-// $(document).ready(function(){
-//   $('.main_slick').slick({
-//     arrows: true, // ← → 表示
-//     dots: true,   // ●●● 表示
-//     infinite: true,
-//     slidesToShow: 3, // デフォルトは3枚表示（PCなど大きい画面）
-//     slidesToScroll: 1,
-//     prevArrow: '<button type="button" class="slick-prev">←</button>',
-//     nextArrow: '<button type="button" class="slick-next">→</button>',
-//     responsive: [
-//       {
-//         breakpoint: 768, // 768px未満になったらこの設定に切り替わる
-//         settings: {
-//           slidesToShow: 1, // スマホでは1枚ずつ表示
-//           slidesToScroll: 1
-//         }
-//       }
-//     ]
-//   });
-// });
-$(document).ready(function() {
+document.querySelectorAll('.toggle-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const item = btn.closest('.qa_item');
+    item.classList.toggle('active');
+  });
+});
+$(function () {
   $('.main_slick').slick({
+    autoplay: true,
+    adaptiveHeight: true,
     arrows: true, // ← → 表示
     dots: true,   // ●●● 表示
     infinite: true,
-    slidesToShow: 1, // デフォルトでは1枚表示
+    slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev">←</button>',
-    nextArrow: '<button type="button" class="slick-next">→</button>',
+    prevArrow: '<img src="/assets/image/arrow-left-circle-fill.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="/assets/image/arrow-right-circle-fill.png" class="slide-arrow next-arrow">',
     responsive: [
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1,  // 768px以下でも1枚表示
+          slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: true
@@ -57,7 +39,7 @@ $(document).ready(function() {
       {
         breakpoint: 1920,
         settings: {
-          slidesToShow: 3,  // 768px以上〜1920px未満で3枚表示
+          slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
           dots: true
